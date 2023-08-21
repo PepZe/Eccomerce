2b580d38e5967475406e90e8b74ca7e8e1cd5ae7
PepZe <pedropaulo.azevedo@hotmail.com.br>
2023-08-21 17:24:13 +0100

    Added controllers to the areas

--- Ecommerce/Areas/Admin/Controllers/ProductController.cs
+++ Ecommerce/Areas/Admin/Controllers/ProductController.cs
@@ -1,11 +1,11 @@
 ﻿using Ecommerce.Domain.Interfaces;
 using Ecommerce.Domain.Model;
-using Ecommerce.Domain.Model.ViewModel;
 using FileIO = System.IO.File;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Ecommerce.Models;
 
-namespace Ecommerce.Controllers
+namespace Ecommerce.Areas.Admin.Controllers
 {
     public class ProductController : Controller
     {
